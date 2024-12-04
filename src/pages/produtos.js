7471f44c57f91068a8612f444317b5b4b42d022a
Gabriela Dellafina <135945872+gabdellafina@users.noreply.github.com>
2024-12-04 13:34:14 -0300

    Update produtos.js

--- src/pages/produtos.js
+++ src/pages/produtos.js
@@ -250,7 +250,7 @@ export default function Produtos() {
         <>
             {/* Navegação */}
             <nav className="flex justify-between p-5 items-center">
-                <a href="/homepage" className="ml-1 flex items-center gap-2">
+                <a href="/index" className="ml-1 flex items-center gap-2">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-4">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
                     </svg>
