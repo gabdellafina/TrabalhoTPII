67bf028d6cf94bc3b8ee089b3230e300779f5683
Gabriela Dellafina <135945872+gabdellafina@users.noreply.github.com>
2024-12-04 13:32:25 -0300

    commit1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,90 +1,39 @@
 import Image from "next/image";
-import { React, useState } from "react";
-import Logistics from '../public/img/Logistics.png';
+import cart from '../public/img/cart.png';
+import store from '../public/img/store.png';
 import '../app/globals.css';
-const [nome, setNome] = useState("");
-const [senha, setSenha] = useState("");
-const [mensagem, setMensagem] = useState("");
-
-const handleSubmit = async (e) => {
-  e.preventDefault();
-
-  try {
-    const response = await fetch("http://localhost:8080/usuarios/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: new URLSearchParams({ nome, senha }).toString(),
-    });
-
-    const textoResposta = await response.text();
-    setMensagem(textoResposta);
-  } catch (error) {
-    setMensagem("Erro ao tentar realizar o login. Tente novamente.");
-  }
-};
 
 export default function Home() {
   return (
     <div className="relative min-h-screen bg-black overflow-hidden">
       <div>
-        <h1 className="text-3xl font-bold text-center mt-16 text-white">Controle de Estoque</h1>
+        <h1 className="text-3xl font-bold text-center mt-10 text-white">Acesse:</h1>
       </div>
+      
+      <div className="flex items-center justify-center h-full mt-16">
+        <a href="/produtos">
+          <div className="bg-white bg-opacity-10 backdrop-blur-lg p-8 rounded-lg shadow-lg max-w-md transition-transform transform hover:scale-105 hover:border-4 hover:border-sky-400 hover:shadow-lg hover:shadow-sky-400 hover:scale-105">
+            <Image
+              src={cart}
+              alt="Produtos"
+              width={200}
+              height={200}
+            />
+            <h2 className="text-2xl font-bold text-center mt-4 text-white">Produtos</h2>
+          </div>
+        </a>
 
-      <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <form
-        onSubmit={handleSubmit}
-        className="bg-white p-8 rounded-lg shadow-md w-full max-w-md"
-      >
-        <h2 className="text-2xl font-bold mb-6 text-gray-800">Login</h2>
-        <div className="mb-4">
-          <label htmlFor="nome" className="block text-gray-700 font-medium mb-2">
-            Nome
-          </label>
-          <input
-            type="text"
-            id="nome"
-            value={nome}
-            onChange={(e) => setNome(e.target.value)}
-            className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none"
-            placeholder="Digite seu nome"
-            required
-          />
-        </div>
-        <div className="mb-6">
-          <label htmlFor="senha" className="block text-gray-700 font-medium mb-2">
-            Senha
-          </label>
-          <input
-            type="password"
-            id="senha"
-            value={senha}
-            onChange={(e) => setSenha(e.target.value)}
-            className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none"
-            placeholder="Digite sua senha"
-            required
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition-colors duration-200"
-        >
-          Entrar
-        </button>
-        {mensagem && (
-          <p className="mt-4 text-center text-gray-700">{mensagem}</p>
-        )}
-      </form>
-    </div>
-
-      <div className="absolute bottom-0 left-0">
-        <Image
-          src={Logistics}
-          alt="Logística"
-          width={500}
-          height={500}
-        />
+        <a href="/lojas">
+          <div className="bg-white bg-opacity-10 backdrop-blur-lg p-8 rounded-lg shadow-lg max-w-md ml-8 transition-transform transform hover:scale-105 hover:border-4 hover:border-sky-400 hover:shadow-lg hover:shadow-sky-400 hover:scale-105">
+            <Image
+              src={store}
+              alt="Lojas"
+              width={200}
+              height={200}
+            />
+            <h2 className="text-2xl font-bold text-center mt-4 text-white">Lojas</h2>
+          </div>
+        </a>
       </div>
     </div>
   );
